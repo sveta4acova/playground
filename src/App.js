a330bbaa96096e57b5ea5ec510650c9b14a89d31
sveta <svetlana.shurla4acova@gmail.com>
2019-10-17 18:47:55 +0300

    learned hook useState

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import StateHook from './components/state-hook';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <StateHook/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
