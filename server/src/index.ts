a5abb236a81a98e7bc4127cdd659589b36d147f8
sveta <svetlana.shurla4acova@gmail.com>
2020-06-01 18:34:20 +0300

    created test express app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello')
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(cookieSession({ keys: ['super'] }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
+});
